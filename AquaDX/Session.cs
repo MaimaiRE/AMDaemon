bd594dcdb5df60984135a0653faa3174e249e9d7
mos9527 <greats3an@gmail.com>
2025-04-15 18:31:26 +0800

    Initial AimeDB implementation

--- AquaDX/Session.cs
+++ AquaDX/Session.cs
@@ -5,6 +5,7 @@ using System.IO;
 using MaimaiRE;
 using Ionic.Zlib;
 using System;
+using System.Collections;
 using UnityEngine.Networking;
 using System.Threading;
 using System.Web;
@@ -14,51 +15,44 @@ using System.Drawing.Printing;
 namespace AMDaemon
 {
     namespace AquaDX
-    {        
+    {
         public class Session : Singleton<Session>
         {
             public static byte[] EncodeAllnet(Dictionary<string, string> data, bool base64 = false)
-            {                
+            {
                 string encoded = "";
                 foreach (var pair in data)
                     encoded += pair.Key + "=" + pair.Value + "&";
-                byte[] bytes = Encoding.UTF8.GetBytes(encoded.TrimEnd('&'));                
+                byte[] bytes = Encoding.UTF8.GetBytes(encoded.TrimEnd('&'));
                 using (MemoryStream ms = new MemoryStream())
                 {
                     // XXX: Could've just used DeflateStream instead?
-                    using (ZlibStream zlibStream = new ZlibStream(ms, CompressionMode.Compress, CompressionLevel.BestCompression))
+                    using (ZlibStream zlibStream =
+                           new ZlibStream(ms, CompressionMode.Compress, CompressionLevel.BestCompression))
                     {
                         zlibStream.Write(bytes, 0, bytes.Length);
                     }
+
                     bytes = ms.ToArray();
                 }
+
                 if (base64)
                     return Encoding.UTF8.GetBytes(Convert.ToBase64String(bytes));
                 return bytes;
             }
-            public string KeychipID { get
-                {
-                    string src = Config.Instance.KeychipID;
-                    if (string.IsNullOrEmpty(src)) return null;
-                    string ret = src.Substring(0, 4) + src.Substring(4, 7);
-                    return ret;
-                }
-            }
+
+            public string KeychipID => MaimaiRE.Config.Instance.EncodedKeychipID;
             public bool IsValid => !string.IsNullOrEmpty(KeychipID);
-            private string _ServerEndpoint;
-            public string GetServerEndpoint()
+
+            private string _ServerEndpoint = "";
+            public string ServerEndpoint => _ServerEndpoint;
+            
+            public IEnumerator SendPowerOnRequestCoroutine(Action<string?> onComplete)
             {
-                if (_ServerEndpoint != null) return _ServerEndpoint;
-                if (string.IsNullOrEmpty(KeychipID)) return null;
-                SendPowerOnRequest();
-                // Logger.Assert(_ServerEndpoint != null, "Login failed.");
-                return _ServerEndpoint;
-            }
-            private bool shouldRetry = true;
-            public void SendPowerOnRequest()
-            {
-                if (!shouldRetry) return;
                 const string endpoint = "/sys/servlet/PowerOn";
+                string url = "http://" + Config.Instance.AquaDX_Host + endpoint;
+
+                Logger.Log($"Sending PowerOn request to: {url}, keychip: {KeychipID}");
 
                 Dictionary<string, string> data = new Dictionary<string, string>
                 {
@@ -66,39 +60,44 @@ namespace AMDaemon
                     { "game_id", "SDGA" },
                     { "ver", "1.50" }
                 };
+
                 byte[] encoded = EncodeAllnet(data, true);
-                string url = Config.Instance.Server + endpoint;
-                try
+                using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
                 {
-                    using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
+                    request.uploadHandler = new UploadHandlerRaw(encoded);
+                    request.downloadHandler = new DownloadHandlerBuffer();
+                    request.SetRequestHeader("Content-Type", "text/plain");
+                    request.timeout = 15;
+
+                    yield return request.SendWebRequest();
+                    while (!request.isDone)  { yield return null; }
+                    if (request.result != UnityWebRequest.Result.Success)
                     {
-                        request.uploadHandler = new UploadHandlerRaw(encoded);
-                        request.downloadHandler = new DownloadHandlerBuffer();
-                        request.SetRequestHeader("Content-Type", "text/plain");
-                        request.SendWebRequest();
-                        // TODO: Async
-                        while (!request.isDone) { Thread.Sleep(100); }
-                        if (request.responseCode != 200)
-                        {
-                            Logger.Error($"Rejected PowerOn request: {request.responseCode}, {request.downloadHandler.text}");
-                            shouldRetry = false;
-                            return;
-                        }
-                        string query = request.downloadHandler.text;
-                        // stat=1&name=&place_id=123&region0=1&region_name0=W&region_name1=X&region_name2=Y&region_name3=Z&country=JPN&nickname=AquaDX&uri=http://aquadx.hydev.org/gs/4_HUJegC1Z6XBQYYtDKyfcKonW6u~GBP/mai2/&host=aquadx.hydev.org&allnet_id=456&client_timezone=+0900&utc_time=2025-03-06T14:26:11Z&setting=&res_ver=3&token=null
-                        var dict = query
-                            .Split('&', StringSplitOptions.RemoveEmptyEntries)
-                            .Select(param => param.Split('='))
-                            .ToDictionary(kv => Uri.UnescapeDataString(kv[0]), kv => Uri.UnescapeDataString(kv[1]));
-                        _ServerEndpoint = dict["uri"];
-                        _ServerEndpoint = _ServerEndpoint.Replace("http://", "");
-                        Logger.Log($"Server endpoint: {_ServerEndpoint}");
+                        Logger.Error($"PowerOn request failed: {request.error},{request.result},{request.responseCode}");
+                        onComplete?.Invoke(null);
+                        yield break;
                     }
-                }
-                catch (Exception e)
-                {
-                    Logger.Error($"Failed to send PowerOn request: {e}");
-                    shouldRetry = false;
+                    string query = request.downloadHandler.text;
+                    var dict = query
+                        .Split('&', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(param => param.Split('='))
+                        .Where(kv => kv.Length == 2) // Add safety check for split result length
+                        .ToDictionary(
+                            kv => Uri.UnescapeDataString(kv[0]),
+                            kv => Uri.UnescapeDataString(kv[1])
+                        );
+
+                    if (dict.TryGetValue("uri", out string serverUri))
+                    {
+                        _ServerEndpoint = serverUri.Replace("http://", "");
+                        Logger.Log($"Server endpoint updated: {_ServerEndpoint}");
+                        onComplete?.Invoke(_ServerEndpoint);   
+                    }
+                    else
+                    {
+                        Logger.Error("Missing uri key.");
+                        onComplete?.Invoke(null);
+                    }
                 }
             }
         }
--- AquaDX/Util.cs
+++ AquaDX/Util.cs
@@ -1,4 +1,5 @@
 
+using System;
 using MaimaiRE;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -11,6 +12,36 @@ namespace AMDaemon
         public static class Utils
         {
             public static bool _AimeStatus;
+
+            public static string EncodeKeychipID(string keychip)
+            {
+                if (string.IsNullOrEmpty(keychip)) return null;
+                keychip = keychip.Replace("-", "");
+                if (keychip.Substring(keychip.Length - 4) == "1337")
+                    keychip = keychip.Substring(0, keychip.Length - 4);
+                return keychip;
+            }
+            
+            private static int GetHexVal(char hex)
+            {
+                int val = (int)hex;
+                return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+            }
+            
+            public static byte[] HexStringToByteArray(string hex)
+            {
+                if (hex.Length % 2 == 1)
+                    throw new ArgumentException("Hex string must have an even number of digits.", nameof(hex));
+
+                byte[] arr = new byte[hex.Length >> 1];
+                for (int i = 0; i < hex.Length >> 1; ++i)
+                {
+                    arr[i] = (byte)((GetHexVal(hex[i << 1]) << 4) + (GetHexVal(hex[(i << 1) + 1])));
+                }
+                return arr;
+            }
+
+
         }
     }
 }
